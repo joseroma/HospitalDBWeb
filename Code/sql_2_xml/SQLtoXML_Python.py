3ed116ba019af273b53f6454ba14accbcd37b2ef
Jose Rodríguez Maldonado <joseroma@uma.es>
2018-10-25 23:45:29 +0200

    REFACTOR BD + SQL2XML script

--- Code/sql_2_xml/SQLtoXML_Python.py
+++ Code/sql_2_xml/SQLtoXML_Python.py
@@ -11,22 +11,58 @@ todaydate = i.strftime('%Y') + i.strftime('%m') + i.strftime('%d')
 
 
 # DB CONNECTION AND QUERY
-db = pymysql.connect(host="hostname", user="username",
-                      passwd="password", db="database") 
-cur = db.cursor() 
-cur.execute('SELECT `user`, `category`, `city`, `post`, `time`, `link` FROM cldata')
-
+db = pymysql.connect(host='localhost', port=3306, user="root",
+                      passwd="admin", db="mydb")
+cur = db.cursor()
+cur.execute('SELECT `*` FROM MEDICO, QUIROFANO')
+print(cur.fetchall())
 # WRITING XML FILE
-root = ET.Element('CLData')
+root = ET.Element('HOSPITAL')
 
 for row in cur.fetchall():
-    mcNode = ET.SubElement(root, "missedConnection")
-    ET.SubElement(mcNode, "user").text = str(row[0])    
-    ET.SubElement(mcNode, "category").text = row[1]
-    ET.SubElement(mcNode, "city").text = row[2]    
-    ET.SubElement(mcNode, "post").text = row[3]    
-    ET.SubElement(mcNode, "time").text = row[4]    
-    ET.SubElement(mcNode, "link").text = row[5]
+    paciente = ET.SubElement(root, "PACIENTES")
+    ET.SubElement(paciente, "num_expediente").text = str(row[0])
+    ET.SubElement(paciente, "nombre").text = str(row[1])
+    ET.SubElement(paciente, "apellido").text = row[2]
+    ET.SubElement(paciente, "edad").text = str(row[3])
+    ET.SubElement(paciente, "sexo").text = row[4]
+    ET.SubElement(paciente, "fecha_ingreso").text = str(row[5])
+    ET.SubElement(paciente, "hora_ingreso").text = str(row[6])
+    ET.SubElement(paciente, "pais_origen").text = row[7]
+    ET.SubElement(paciente, "fumador").text = row[8]
+    ET.SubElement(paciente, "URGENCIA_id_urgencia").text = str(row[9])
+    #URGENCIA
+    urgencia = ET.SubElement(paciente, "URGENCIA")
+    ET.SubElement(urgencia, "id_urgencia").text = str(row[0])
+    ET.SubElement(urgencia, "causa").text = str(row[1])
+    ET.SubElement(urgencia, "gravedad").text = row[2]
+    ET.SubElement(urgencia, "uso_ambulancia").text = str(row[3])
+    ET.SubElement(urgencia, "necesita_operacion").text = row[4]
+    ET.SubElement(urgencia, "ingreso").text = str(row[5])
+    ET.SubElement(urgencia, "ESPECIALIDAD_nombre_especialidad").text = str(row[6])
+    ET.SubElement(urgencia, "OPERACION_id_operacion").text = row[7]
+    # OPERACION
+    operacion = ET.SubElement(urgencia, "OPERACION")
+    ET.SubElement(operacion, "id_operacion").text = str(row[0])
+    ET.SubElement(operacion, "hora").text = str(row[1])
+    ET.SubElement(operacion, "cirujano").text = row[2]
+    ET.SubElement(operacion, "QUIROFANO_id_quirofano").text = str(row[3])
+    #ESPECIALIDAD
+    especialidad = ET.SubElement(urgencia, "ESPECIALIDAD")
+    ET.SubElement(especialidad, "nombre_especialidad").text = str(row[0])
+    # quirofano
+    quirofano = ET.SubElement(operacion, "QUIROFANO")
+    ET.SubElement(quirofano, "id_quirofano").text = str(row[0])
+    ET.SubElement(quirofano, "planta").text = row[1]
+    ET.SubElement(quirofano, "puerta").text = row[2]
+    #MEDICO
+    medico = ET.SubElement(especialidad, "MEDICO")
+    ET.SubElement(medico, "id_medico").text = str(row[0])
+    ET.SubElement(medico, "nombre_medico").text = row[1]
+    ET.SubElement(medico, "ESPECIALIDAD_nombre_especialidad").text = row[2]
+
+
+
 
 
 # CLOSE CURSOR AND DATABASE
