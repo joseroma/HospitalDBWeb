5ba54601e21812fcc04d8282ed4f53102d22bb50
Jose Rodríguez Maldonado <joseroma@uma.es>
2019-02-04 23:10:11 +0100

    project tune up

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='HospitalDBWeb',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/nairachiclana/HospitalDBWeb',
+    license='MIT',
+    author='joseroma & nairachiclana',
+    description='Trabajo de la asignatura de Estandares de datos abiertos e integración de datos.'
+)
